6b9ece5c2b43a06ba648f5a857226c583b2c4500
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-06-18 20:03:58 +0530

    add

--- student-app/resources/views/layout.blade.php
+++ student-app/resources/views/layout.blade.php
@@ -101,9 +101,9 @@
                 </div>
 
                 <!-- Page content -->
-                <div class="content">
+               
                     @yield('content')
-                </div>
+                
             </div>
         </div>
     </div>
--- student-app/routes/web.php
+++ student-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout');
 });
+
+Route::resource('/students', StudentController::class);
