418dd67044eea7f476591edefecfc51f1d05b1d2
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-06-30 19:07:24 +0530

    add

--- student-app/resources/views/batches/create.blade.php
+++ student-app/resources/views/batches/create.blade.php
@@ -12,12 +12,14 @@
 
       <label>Course Name</label></br>
       <!-- <input type="text" name="course_id" id="course_id" class="form-control"></br> -->
-      <select name="course_id" id="course_id" class="form control">
-        @foreach($courses as $id -> $name)
+      <select name="course_id" id="course_id" class="form-control">
+
+        @foreach($courses as $id => $name)
         <option value="{{$id}}">{{$name}}</option>
         @endforeach
-      </select></br>
 
+      </select></br>
+ 
       <label>Start Date</label></br>
       <input type="text" name="start_date" id="start_date" class="form-control"></br>
       <input type="submit" value="Save" class="btn btn-success"></br>
